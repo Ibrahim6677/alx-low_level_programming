edf656815a8681f6305beec285cdcace557549c7
ibrahim <ibrahimsaad6677@gmail.com>
2023-09-07 04:50:20 -0700

    finshed

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,14 @@
+#include <stido.h>
+
+/**
+ * main - Entry point
+ * Description: A C progam that prints with pritf function.
+ *
+ * Return: Always 0 (Succes)
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
