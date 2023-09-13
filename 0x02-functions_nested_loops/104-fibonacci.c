4ca698412e26fa4e91b8a4a8c8e16c08bda6d9f6
ibrahim <ibrahimsaad6677@gmail.com>
2023-09-12 18:12:28 -0700

    finshed

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -41,11 +41,11 @@ int main(void)
 	{
 		if (f1o > 0)
 			printf("%lu", f1o);
-		initial0s = numLenght(mx) - 1 - numLength(f1);
+		initial0s = numLength(mx) - 1 - numLength(f1);
 
 		while (f1o > 0 && initial0s > 0)
 		{
-			printf("%D", 0);
+			printf("%d", 0);
 			initial0s--;
 		}
 
