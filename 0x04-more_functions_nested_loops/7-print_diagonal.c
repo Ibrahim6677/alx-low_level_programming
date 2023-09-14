64d95150fabcc0dd059bc6c2c778827b5f00c43f
ibrahim <ibrahimsaad6677@gmail.com>
2023-09-14 07:08:11 -0700

    finshed

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -13,13 +13,14 @@ void print_diagonal(int n)
 
 	if (n <= 0)
 		_putchar('\n');
+		return;
 	else
 	{
 		for (postn = 1; postn < n; postn++)
 		{
 			for (space = 1; space < postn; space++)
 				_putchar(' ');
-			_putchar('\\');
+			_putchar('92');
 			_putchar('\n');
 		}
 	}
