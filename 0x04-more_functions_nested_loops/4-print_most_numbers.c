e421909df432bafe1714b688eefbd5e9b1ffb28e
ibrahim <ibrahimsaad6677@gmail.com>
2023-09-14 06:07:51 -0700

    finshed

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -2,9 +2,8 @@ include "main.h"
 
 /**
  * print_most_numbers - print 0 - 9 apart
- *			from 2 and 4 you can
- *			only use _putchar twice
- *
+ *		from 2 and 4 you can
+ *		only use _putchar twice
  * Return: Always 0 (Success)
 */
 
