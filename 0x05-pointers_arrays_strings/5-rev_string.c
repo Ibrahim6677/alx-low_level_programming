9454cff894d62167e2b930a102725a24002ba716
ibrahim <ibrahimsaad6677@gmail.com>
2023-09-18 06:33:26 -0700

    finshed

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * rev_string - reverses a string
+ *
+ * @s: string parameter input
+ *
+ * Return: Nothing
+*/
+
+void rev_string(char *s)
+{
+	int l, i;
+	char temp;
+
+	for (l = 0; s[l] != '\0'; ++1)
+		;
+
+	for (i = 0; i < l; i++)
+	{
+		temp = s[i];
+		s[i] = s[l - 1 - i];
+		s[l - 1 - i] = temp;
+	}
+
+}
