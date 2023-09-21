4536eacb87f0cc25f59f7d438edabdfbda7fe03e
ibrahim <ibrahimsaad6677@gmail.com>
2023-09-21 11:34:33 +0000

    finshed

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -1,9 +1,9 @@
 #include "main.h"
 
 /**
-* rot13 - encodes a string using rot13
-* @s: input string
-* return: encoded string
+ * rot13 - encodes a string using rot13
+ * @s: input string
+ * Return: encoded string
 */
 
 char *rot13(char *s)
