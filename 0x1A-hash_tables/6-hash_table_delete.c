6ab50e40b1e696e50ab740b6ef4d1dd08c167391
ibrahim <ibrahimsaad6677@gmail.com>
2023-12-22 09:46:34 +0000

    end

--- 0x1A-hash_tables/6-hash_table_delete.c
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -11,7 +11,7 @@ void hash_table_delete(hash_table_t *ht)
 	unsigned long int i;
 	hash_node_t *next;
 
-	if (ht === NULL || ht->array == NULL || ht->size == 0)
+	if (ht == NULL || ht->array == NULL || ht->size == 0)
 		return;
 	for (i = 0; i < ht->size; i++)
 	{
