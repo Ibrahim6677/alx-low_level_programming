f253c088c0955a63f1bb84b3edd863e90ae6a2cd
ibrahim <ibrahimsaad6677@gmail.com>
2024-04-12 14:04:07 -0700

    sear

--- 0x1E-search_algorithms/100-jump.c
+++ 0x1E-search_algorithms/100-jump.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include "search_algos.h"
 /**
- * jump_search - function that searches for a value in a sorted array of integers 
+* jump_search -function that searches for a value in a sorted array of integers
  * @array:  pointer to the first element of the array to search in
  * @size: number of elements in array
  * @value: value to search for
@@ -10,15 +10,15 @@
 int jump_search(int *array, size_t size, int value)
 {
 	if (array == NULL || size == 0)
-		return -1;
+		return (-1);
 
-	// Define jump step using square root of array size
+	/* Define jump step using square root of array size*/
 	size_t step = (size_t)sqrt(size);
 	size_t prev = 0;
 
 	printf("Value checked array[%ld] = [%d]\n", prev, array[prev]);
 
-	// Jump through the array
+	/* Jump through the array*/
 	while (array[step] < value && step < size)
 	{
 		prev = step;
@@ -29,13 +29,13 @@ int jump_search(int *array, size_t size, int value)
 
 	printf("Value found between indexes [%ld] and [%lu]\n", prev, step);
 
-	// Linear search within the jump range
+	/* Linear search within the jump range*/
 	for (size_t i = prev; i <= step && i < size; i++)
 	{
 		printf("Value checked array[%ld] = [%d]\n", i, array[i]);
 		if (array[i] == value)
-			return i;
+			return (i);
 	}
 
-	return -1;
+	return (-1);
 }
